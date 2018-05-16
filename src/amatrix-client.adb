ffa4467a8bcd41ee1d597407a79ecfa93ca4e1d1
Johannes Kliemann <jk@jkliemann.de>
2018-05-16 14:54:53 +0200

    login at runtime, not instantiation

--- src/amatrix-client.adb
+++ src/amatrix-client.adb
@@ -7,24 +7,107 @@ package body Amatrix.Client is
    -- Login --
    -----------
 
-   function Login (
-                   User : String;
-                   Pass : String
-                  ) return String
+   Invalid_Token : Access_Token_Type := (others => Character'Val (0));
+   Access_Token : Access_Token_Type;
+   Invalid_Homeserver : Homeserver_Type := (others => Character'Val (0));
+   Homeserver : Homeserver_Type;
+
+   procedure Login (
+                    User : String;
+                    Pass : String
+                   )
    is
-      Response : String := Http.Post (Homeserver & "/_matrix/client/r0/login",
+      Hs       : String := Get_Homeserver (User);
+      Response : String := Http.Post (Hs
+                                      & "/_matrix/client/r0/login",
                                       "{""initial_device_display_name"": ""amatrix test"","
                                       & """password"": """ & Pass & ""","
                                       & """type"": ""m.login.password"","
-                                      & """user"": """ & User & """}");
+                                      & """user"": """ & Get_Username (User)
+                                      & """}");
       package Response_Object is new JWX.JSON (Response);
-      M : Response_Object.Match_Type;
+      M        : Response_Object.Match_Type;
    begin
+      Set_Homeserver (Hs);
       Response_Object.Parse (M);
-      return Response_Object.Get_String (Response_Object.Query_Object ("access_token"));
+      Set_Access_Token (Response_Object.Get_String (Response_Object.Query_Object ("access_token")));
    end Login;
 
-   Access_Token : String := Login(Username, Password);
+   procedure Set_Access_Token (
+                               Token : String
+                              )
+   is
+   begin
+      Access_Token := Invalid_Token;
+      for I in Integer range 0 .. Token'Length - 1 loop
+         Access_Token (Access_Token'First + I) := Token (Token'First + I);
+      end loop;
+   end;
+
+   function Get_Access_Token return String
+   is
+   begin
+      for I in Access_Token'Range loop
+         if Access_Token (I) = Character'Val (0) then
+            return String (Access_Token (1 .. I - 1));
+         end if;
+      end loop;
+      return String (Invalid_Token);
+   end Get_Access_Token;
+
+   procedure Set_Homeserver (
+                             Server : String
+                            )
+   is
+   begin
+      Homeserver := Invalid_Homeserver;
+      for I in Server'Range loop
+         Homeserver (I) := Server (I);
+      end loop;
+   end Set_Homeserver;
+
+   function Get_Homeserver return String
+   is
+   begin
+      for I in Homeserver'Range loop
+         if Homeserver (I) = Character'Val (0) then
+            return String (Homeserver (1 .. I - 1));
+         end if;
+      end loop;
+      return String (Invalid_Homeserver);
+   end Get_Homeserver;
+
+   function Get_Username (
+                          User : String
+                         ) return String
+   is
+   begin
+      if User (User'First) = '@' then
+         for I in User'Range loop
+            if User (I) = ':' then
+               return User (User'First + 1 .. I - 1);
+            end if;
+         end loop;
+      else
+         return User;
+      end if;
+   end Get_Username;
+
+   function Get_Homeserver (
+                            User : String
+                           ) return String
+   is
+   begin
+      if User (User'First) = '@' then
+         for I in User'Range loop
+            if User (I) = ':' then
+               return "https://" & User (I + 1 .. User'Last);
+            end if;
+         end loop;
+      else
+         return "https://matrix.org";
+      end if;
+   end Get_Homeserver;
 
    function Image (
                    Value : Transaction_Type
@@ -35,9 +118,9 @@ package body Amatrix.Client is
                    ) return Integer
       is
          Length : Integer := 0;
-         V : Natural := Natural (Val);
+         V      : Natural := Natural (Val);
       begin
-         Lloop:
+         Lloop :
          loop
             Length := Length + 1;
             if V > 0 then
@@ -48,7 +131,7 @@ package body Amatrix.Client is
          return Length;
       end Len;
 
-      V : Natural := Natural (Value);
+      V   : Natural := Natural (Value);
       Img : String (1 .. Len (Value)) := (others => 'X');
    begin
       for I in reverse Img'Range loop
@@ -63,7 +146,7 @@ package body Amatrix.Client is
             when 7 => Img (I) := '7';
             when 8 => Img (I) := '8';
             when 9 => Img (I) := '9';
-            when others => Img(I) := 'X';
+            when others => Img (I) := 'X';
          end case;
          V := V / 10;
       end loop;
@@ -81,15 +164,15 @@ package body Amatrix.Client is
                            Room_Id : Room_Type
                           )
    is
-      Response : String := Http.Put (Homeserver &
-                                     "/_matrix/client/r0/rooms/"
+      Response : String := Http.Put (Get_Homeserver &
+                                       "/_matrix/client/r0/rooms/"
                                      & String (Room_Id)
                                      & "/send/m.room.message/"
                                      & Image (Transaction_Id),
                                      "{ ""body"":"""
                                      & Message
                                      & """, ""msgtype"":""m.text""}",
-                                    Access_Token);
+                                     Get_Access_Token);
    begin
       null;
    end Send_Message;
--- src/amatrix-client.ads
+++ src/amatrix-client.ads
@@ -1,24 +1,43 @@
 generic
-   Username : String;
-   Password : String;
-   Homeserver : String;
+   Client_Name : String;
 package Amatrix.Client is
 
    type Transaction_Type is new Positive;
    type Room_Type is new String;
-   type Access_Token_Type is new String;
+   type Access_Token_Type is array (Integer range 1 .. 8192) of Character;
+   type Homeserver_Type is array (Integer range 1 .. 512) of Character;
 
    procedure Send_Message (
                            Message : String;
                            Room_Id : Room_Type
                           );
 
+   procedure Login (
+                    User : String;
+                    Pass : String
+                   );
+
 private
 
-   function Login (
-                   User : String;
-                   Pass : String
-                  ) return String;
+   procedure Set_Access_Token (
+                               Token : String
+                              );
+
+   function Get_Access_Token return String;
+
+   procedure Set_Homeserver (
+                             Server : String
+                            );
+
+   function Get_Homeserver return String;
+
+   function Get_Username (
+                          User : String
+                         ) return String;
+
+   function Get_Homeserver (
+                            User : String
+                           ) return String;
 
    function Image (
                    Value : Transaction_Type
